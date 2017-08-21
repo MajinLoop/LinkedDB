609d500de6bc3c0b34a0087de6d8e4c67e55fa61
aleja <aleja@Loop-DESKTOP-8FLS2E1.Home>
2017-08-21 11:47:33 -0600

    Commit No.3 -Estructuras creadas: Lista_simple, Lista Doble, Lista Doble Circular y sus respectivos Nodos. - Herramientas creadas: VG(Variables Globales), ManejadorDeArchivos, Serializador.

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,12 +1,25 @@
 package com.main;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+
+import com.estructuras.Lista_Simple;
+import com.herramientas.Serializador;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException
 	{
+		
+		Lista_Simple<Integer> l = new Lista_Simple<>();
+		l = (Lista_Simple<Integer>)Serializador.desSerializar("l.json",l);
+		l.mostrar(false);
+		//
 		launch(args);
 	}
 
